025abe0b6c84bec588d1ece715ab64a8b2d88d31
qixi.hhy <qixi.hhy@taobao.com>
2013-12-14 12:03:45 +0800

    a

--- scripts/words_count.sh
+++ scripts/words_count.sh
@@ -2,4 +2,4 @@
 
 input=$1
 
-iconv -f utf-16le -t utf-8 $input | awk '{{gsub("（", "("); gsub("）", ")"); gsub(/\(.*\)\/w/, ""); print$0}}' | while read LINE; do echo $LINE | wc -w; done  > $input.words.count
+awk '{{gsub("（", "("); gsub("）", ")"); gsub(/\(.*\)\/w/, ""); print$0}}' $input | while read LINE; do echo $LINE | wc -w; done  > $input.words.count
